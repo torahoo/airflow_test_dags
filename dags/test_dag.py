54aaf9ef94308990de35cf733ec04f6b664da2d3
tae yun kim <onemiddle@huniverse.co.kr>
2024-12-11 21:02:17 +0900

    change hourly

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     "sample_dag",
     start_date=datetime(2021, 1, 1),
-    schedule_interval="@daily",
+    schedule_interval="@hourly",
     catchup=False,
 ) as dag:
     task_1 = BashOperator(task_id="task_1", bash_command="echo 'Hello World from DAG task 1'")
