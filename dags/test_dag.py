853d29c43bf3fe7f64bb9578617ae4d6bd3b1ec5
tae yun kim <onemiddle@huniverse.co.kr>
2024-12-11 20:10:17 +0900

    add test_dag.py

---
+++ dags/test_dag.py
@@ -0,0 +1,11 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    "sample_dag",
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    task_1 = BashOperator(task_id="task_1", bash_command="echo 'Hello World!!! from DAG task 1'")
