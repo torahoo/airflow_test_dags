06c3b102752797553640495c341d0f629c96246f
tae yun kim <onemiddle@huniverse.co.kr>
2024-12-12 15:39:37 +0900

    [2024-12-12]  - 수정 6

--- dags/db_connect_test_dag.py
+++ dags/db_connect_test_dag.py
@@ -5,6 +5,9 @@ from datetime import datetime
 # from dags.python_packages.test_connect_to_db.main import main
 from python_packages.test_connect_to_db.main import main
 
+def func():
+    main()
+
 with DAG(
     "db_connect_test_dag",
     start_date=datetime(2021, 1, 1),
@@ -14,5 +17,5 @@ with DAG(
     # connect_test01 = BashOperator(task_id="task_1", bash_command="echo 'Hello World from DAG task 1'")
     connect_test01 = PythonOperator(
         task_id="connect_test01" ,
-        python_callable=main
+        python_callable=func
     )
