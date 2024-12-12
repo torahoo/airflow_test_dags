739a378100e3b1d2b5899446d96736e75c6894a2
tae yun kim <onemiddle@huniverse.co.kr>
2024-12-12 15:18:59 +0900

    [2024-12-12]  - 수정 4

--- dags/python_packages/test_connect_to_db/src/config/path_config.py
+++ dags/python_packages/test_connect_to_db/src/config/path_config.py
@@ -2,6 +2,6 @@
 class Path:
     # dags 폴더의 dags 파일 상위의 프로젝트 루트를 중심으로 Path 형성
     config_path = "python_packages/test_connect_to_db/src/config"
-    log_path = "dags/python_packages/test_connect_to_db/src/log"
-    function_path = "dags/python_packages/test_connect_to_db/src/function"
+    log_path = "python_packages/test_connect_to_db/src/log"
+    function_path = "python_packages/test_connect_to_db/src/function"
     query_path = "python_packages/test_connect_to_db/src/query"
